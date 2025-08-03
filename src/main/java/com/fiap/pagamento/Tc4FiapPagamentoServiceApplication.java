302c46e7e2e8a9155717b3ac8ee98a66fbb746fc
Demostenis Villar <ribeiros@outlook.com>
2025-08-03 13:50:06 +0100

    Add initial project structure with Spring Boot, Docker, and PostgreSQL setup

---
+++ src/main/java/com/fiap/pagamento/Tc4FiapPagamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc4FiapPagamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tc4FiapPagamentoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/pagamento/Tc4FiapPagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc4FiapPagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
